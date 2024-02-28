403eb6a64f8571b74d029d294a486932adac874d
Nino Nonikashvili <nonikashvilinino8799@gmail.com>
2024-02-28 23:28:04 +0400

    wip: create tasks
    1.create migration, model, controller and seeder for tasks. 2. add grouped routes for crud operations 3. created relations for user and task tables

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [TaskController::class, 'index'])->middleware('auth');
-Route::post('/', [TaskController::class, 'index'])->middleware('auth');
+Route::controller(TaskController::class)->group(function () {
+	Route::middleware(['auth'])->group(function () {
+		Route::get('/tasks/{task}', 'show')->name('show-single-task');
+		Route::get('/create', 'create');
+		Route::post('/store', 'store')->name('store-task');
+		Route::get('/edit', 'edit')->name('edit-task');
+		Route::post('/update', 'update')->name('update-task');
+		Route::post('/destroy', 'destroy')->name('destroy-task');
+	});
+});
 
 Route::get('login', [LoginController::class, 'index'])->middleware('guest')->name('login');
 Route::post('login', [LoginController::class, 'login'])->middleware('guest')->name('login');
