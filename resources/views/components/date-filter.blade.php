663ba0de8ad48d13966e23dec4fc8a13265d40a6
Nino Nonikashvili <nonikashvilinino8799@gmail.com>
2024-02-28 23:31:14 +0400

    wip: markup for dashboard page
    1. add blade component for table data row 2. published pagination file and customized pagination links 3. add additional blade files

---
+++ resources/views/components/date-filter.blade.php
@@ -0,0 +1,5 @@
+
+<div class="flex flex-col">
+    <x-icons.up-arrow />
+    <x-icons.down-arrow />
+</div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
     <main class="flex gap-9 h-full">
 
         <x-admin-section />
-        <section class="grow mt-24">
+        <section class="grow mt-24 flex flex-col">
             <header class="flex justify-between items-end">
                 <h1 class="ml-10 text-3xl font-bold leading-4  text-gray-900">{{__('dashboard.your_tasks')}}</h1>
                 <div class="flex gap-4 align-center">
@@ -16,6 +16,48 @@
                 </button>
                 </div>
             </header>
+            <div class="grow px-4 mt-4 h-2/3">
+                <!-- table start -->
+                @if(count($tasks)) 
+                <table class="min-w-full divide-y divide-gray-200">
+                    <thead >
+                        <tr>
+                            <th scope="col" class="pl-6 py-8 max-w-[18rem] overflow-hidden whitespace-nowrap text-left text-lg font-medium text-black">{{__('dashboard.task_name')}}</th>
+                            <th scope="col" class="pl-6 py-8 max-w-[21rem] overflow-hidden whitespace-nowrap text-left text-lg font-medium text-black">{{__('dashboard.description')}}</th>
+                            <th scope="col" class="pl-6 py-8 max-w-[8rem] overflow-hidden whitespace-nowrap">
+                                <div class="flex gap-1">
+                                    <span class="text-left text-lg font-medium text-black">{{__('dashboard.created_at')}}</span>
+                                    <x-date-filter />
+                                </div>
+                                
+                            </th>
+                            <th scope="col" class="pl-6 py-8 max-w-[8rem] overflow-hidden whitespace-nowrap">
+                                <div class="flex gap-1">
+                                    <span class="text-left text-lg font-medium text-black">{{__('dashboard.due_date')}}</span>
+                                    <x-date-filter />
+                                </div>   
+                                
+                            </th>
+                            <th scope="col" class="pl-6 py-8 max-w-[15rem] overflow-hidden whitespace-nowrap text-left text-lg font-medium text-black">{{__('dashboard.actions')}}</th>
+                        </tr>
+                    </thead>
+                   
+                    <tbody class="bg-white divide-y divide-gray-200">
+                        @foreach($tasks as $task)
+                        <x-table-data-row :task="$task"/>
+
+                        @endforeach
+                    </tbody>
+                    
+
+                   
+                </table>
+                {{$tasks->onEachSide(1)->links()}}
+                @endif
+
+                <!-- table end -->
+            </div>
+            <x-lan-switcher  />
         </section>
     </main>
 </x-layout>
